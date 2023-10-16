f6327d15762625c155853589dc560ad107ff079d
IvanCarreras <carreras.ivan.perez@gmail.com>
2023-10-16 18:23:14 +0200

    desde-casa

--- header.php
+++ header.php
@@ -6,21 +6,43 @@
   <meta name="viewport" content="width=device-width, initial-scale=1" />
   <title>Hotel management System</title>
 </head>
-<link rel="stylesheet" type="text/css" href="./style.css">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+
+<!-- <link rel="stylesheet" type="text/css" href="./style.css"> -->
 
 <body>
   <header>
-    <div class="menu">
-      <div id="logo">
-        <a href="/student034/dwes/index.php"><img src="imagenes/logo.png" alt="">
-          <h1>Reshi's hotel</h1>
-        </a>
+    <nav class="navbar bg-danger">
+      <div class="container">
+        <div class="row">
+          <!-- Logo mas titulo -->
+          <div class="col-lg-6">
+            <a href="/student034/dwes/index.php"><img src="imagenes/logo.png" alt="" width="50px">
+              <h1>Reshi's hotel</h1>
+            </a>
+          </div>
+          <div class="col-lg-2"><button>Sign in</button></div>
+          <div class="col-lg-2"><select name="Rooms">
+              <option value=""><a href="/student034/dwes/forms/rooms/form_rooms_select.php">Select</a></option>
+              <option value="">Insert</option>
+              <option value="">Update</option>
+              <option value="">Delete</option>
+            </select></div>
+          <div class="col-lg-2">
+            <button>Contactanos</button>
+          </div>
+        </div>
       </div>
-      <ul>
-        <li><a href="">Sign in</a></li>
-        <li><a href="/student034/dwes/forms/rooms/form_rooms_select.php">Rooms</a></li>
-        <li><a href="">Contactanos</a></li>
-      </ul>
+
+    </nav>
 
     </div>
+
+    <ul>
+      <li><a href="">Sign in</a></li>
+      <li><a href="/student034/dwes/forms/rooms/form_rooms_select.php">Rooms</a></li>
+      <li><a href="">Contactanos</a></li>
+    </ul>
+
+
   </header>
