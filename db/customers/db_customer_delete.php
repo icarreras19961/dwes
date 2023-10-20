857eec557df7be97a1f071aa08d6fccb5a97da2b
Ivan <icarreras19961@iesjoanramis.org>
2023-10-20 21:34:28 +0400

    20/10 hecho todo lo de los customers

---
+++ db/customers/db_customer_delete.php
@@ -0,0 +1,15 @@
+<?php
+if (isset($_POST['submit'])) {
+  include($_SERVER['DOCUMENT_ROOT'].'/student034/dwes/db/db_connection.php');
+  
+  $client_id = $_POST['client_id'];
+
+  $sql = "DELETE FROM 034_clients WHERE client_id = $client_id";
+  if (mysqli_query($conn, $sql)){
+    echo 'Cliente borrado correctamente'; 
+  }else{
+    echo 'Algo salio mal '. mysqli_error($conn);
+  };
+  
+
+}
--- forms/customers/form_customer_delete.php
+++ forms/customers/form_customer_delete.php
@@ -1,25 +1,28 @@
 <?php
 include($_SERVER['DOCUMENT_ROOT'] . '/student034/dwes/header.php');
 ?>
+<div class="m-2">
+  <?php
+  if (isset($_POST['submit'])) {
+    include($_SERVER['DOCUMENT_ROOT'] . '/student034/dwes/db/db_connection.php');
 
-<?php
-if(isset($_POST['submit'])) {
-  include($_SERVER['DOCUMENT_ROOT'] . '/student034/dwes/db/db_connection.php');
+    $client_id = $_POST['client_id'];
 
-  $client_id = $_POST['client_id'];
+    $sql = "SELECT * FROM 034_clients WHERE client_id = $client_id";
+    $resultado = mysqli_query($conn, $sql);
+    $muestra = mysqli_fetch_all($resultado, MYSQLI_ASSOC);
 
-  $sql = "SELECT * FROM 034_clients WHERE client_id = $client_id";
-  $resultado = mysqli_query($conn, $sql);
-  $muestra = mysqli_fetch_all($resultado, MYSQLI_NUM);
-
-  print_r($muestra);
-}
-?>
+    echo 'Seguro que quieres eliminar a: ';
+    echo $muestra[0]['client_name'].' '.$muestra[0]['client_surname'];
+  }
+  ?>
+</div>
 
 <div class="m-4">
-  <form action="/student034/dwes/forms/customers/form_customer_delete.php" method="post">
-
-    <label><input type="submit" value="delete"></label>
+  <form action="/student034/dwes/db/customers/db_customer_delete.php" method="POST">
+    <label><input type="hidden" name="client_id" value="<?php echo $_POST['client_id']; ?>"></label>
+    <label><input type="submit" name="submit" value="Si"></label>
+    <label><button><a href="/student034/dwes/index.php">No</a></button></label>
 
   </form>
 </div>
