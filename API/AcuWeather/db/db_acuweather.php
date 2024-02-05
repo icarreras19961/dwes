51fa145bd5a5c44d6f0bd347db797a75c07502c3
Ivan <icarreras19961@iesjoanramis.org>
2024-02-05 16:20:52 +0100

    maquetando el tiempo

--- API/AcuWeather/db/db_acuweather.php
+++ API/AcuWeather/db/db_acuweather.php
@@ -1,26 +1,38 @@
 <?php
 $wheather = $_SERVER['DOCUMENT_ROOT'] . "/student034/dwes/API/AcuWeather/Files/weather.json";
-$fileWeather = fopen($wheather, "r");
+$icons = $_SERVER['DOCUMENT_ROOT'] . "/student034/dwes/API/AcuWeather/Files/weather_Icon.json";
 
-if (file_exists($wheather)) {
-  $theFile = json_decode(fread($fileWeather, filesize($wheather)), true);
+
+$theFile = json_decode(file_get_contents($wheather), true);
+$theFile = json_decode(file_get_contents($wheather), true);
+
+$theFileIcons = json_decode(file_get_contents($icons), true);
+
 
 ?>
 <br>
-  <div style="display:flex; justify-content:center;">
-
-    <div style="width:500px; background-color:aliceblue; border-radius:10px">
-      <h1>El Tiempo</h1>
-      <hr>
+<div style="display:flex; justify-content:center;">
+  <div style="width:500px; background-color:aliceblue; border-radius:10px">
+    <h1>The weather</h1>
+    <hr>
+    <div style="background-color: white;border-radius:10px;margin: 5px; text-align:center;">
       <?php
-      print_r(
-
-        "Temperature " . $theFile[0]["Temperature"]["Metric"]["Value"] . " " . $theFile[0]["Temperature"]["Metric"]["Unit"] . " " . $theFile[0]["WeatherText"]
-      );
+      foreach ($theFileIcons as $theFileIcon) :
+        if ($theFile[0]["WeatherIcon"] == $theFileIcon["IconNumber"]) {
       ?>
-
+          <img src="<?php echo $theFileIcon["icon"]; ?>" alt="">
+      <?php
+        }
+      endforeach;
+      echo "Temperatura" . $theFile[0]["Temperature"]["Metric"]["Value"] . " " .  "º". $theFile[0]["TemperatureSummary"]["Past6HourRange"]["Maximum"]["Metric"]["Unit"];
+      echo "</br>";
+      echo "Max " . $theFile[0]["TemperatureSummary"]["Past6HourRange"]["Maximum"]["Metric"]["Value"] . " " .  "º" . $theFile[0]["TemperatureSummary"]["Past6HourRange"]["Maximum"]["Metric"]["Unit"];
+      echo "</br>";
+      echo "Min " . $theFile[0]["TemperatureSummary"]["Past6HourRange"]["Minimum"]["Metric"]["Value"] . " " . "º" .  $theFile[0]["TemperatureSummary"]["Past6HourRange"]["Minimum"]["Metric"]["Unit"];
+      ?>
     </div>
   </div>
+</div>
 <?php
-}
+
 //TODO: Falta maquetar el tiempo 
