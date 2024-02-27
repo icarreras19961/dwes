499c381da3e1019a9927b2bbfa9a79f0ab6780a8
Ivan <icarreras19961@iesjoanramis.org>
2024-02-27 21:10:07 +0100

    sigo con extras

--- forms/reservations/form_reservation_service_update.php
+++ forms/reservations/form_reservation_service_update.php
@@ -75,18 +75,30 @@ include($_SERVER['DOCUMENT_ROOT'] . '/student034/dwes/header.php')
   // El extra del spa esta acabado solo queda clonar esto en los demas users
   let spa = document.getElementById("spa");
   let spa_date = document.getElementById("spa_date");
-
+  let spaJson;
+  let spaJsonPiece;
   spa.addEventListener('click', (e) => {
     if (e.target.checked == true) {
       spa_date.style.display = "block";
       sumaTotal = sumaTotal + 25;
-      let spaJson = {
-        "Date Time": spa_date.value,
-        "Initial Price": 25 
-      }
+      spa_date.addEventListener("change", (e) => {
+        spaJsonPiece = {
+          "Date Time": spa_date.value,
+          "Initial Price": 25
+        };
+        spaJson = {
+          "Spa": [spaJsonPiece]
+        };
+        console.log(spaJson);
+      })
+
     } else {
+      spaJson = {
+        "Spa": null
+      }
       sumaTotal = sumaTotal - 25;
       spa_date.style.display = "none";
+      console.log(spaJson);
     }
     console.log(sumaTotal);
     importe.innerText = sumaTotal;
@@ -94,14 +106,28 @@ include($_SERVER['DOCUMENT_ROOT'] . '/student034/dwes/header.php')
 
   let gym = document.getElementById("gym");
   let gym_date = document.getElementById("gym_date");
-
+  let gymJson;
+  let gymJsonPiece;
   gym.addEventListener("click", (e) => {
     if (e.target.checked == true) {
       gym_date.style.display = "block";
       sumaTotal = sumaTotal + 25;
+      gym_date.addEventListener("change", (e) => {
+        gymJsonPiece = {
+          "Date Time": gym_date.value,
+          "Initial Price": 10
+        };
+        gymJson = {
+          "Gym": [gymJsonPiece]
+        };
+        console.log(gymJson);
+      });
     } else {
-      sumaTotal = sumaTotal - 25;
+      sumaTotal = sumaTotal - 10;
       gym_date.style.display = "none";
+      gymJson = {
+        "Gym": null
+      }
     }
     console.log(sumaTotal);
     importe.innerText = sumaTotal;
@@ -109,40 +135,63 @@ include($_SERVER['DOCUMENT_ROOT'] . '/student034/dwes/header.php')
 
   let hourse = document.getElementById("hourse");
   let hourse_date = document.getElementById("hourse_date");
-  let pHeNum = document.getElementById("pHeNum");
-
+  let hourseJsonPiece;
+  let hourseJson;
   hourse.addEventListener("click", (e) => {
     if (e.target.checked == true) {
       sumaTotal = sumaTotal + 50;
       hourse_date.style.display = "block";
+      hourse_date.addEventListener("change", (e) => {
+        hourseJsonPiece = {
+          "Date Time": hourse_date.value,
+          "Initial Price": 50
+        };
+        hourseJson = {
+          "Horse_expedition": [hourseJsonPiece]
+        };
+        console.log(hourseJson);
+      });
     } else {
       sumaTotal = sumaTotal - 50;
       hourse_date.style.display = "none";
+      hourseJson = {
+        "Horse_expedition": null
+      }
     }
     console.log(sumaTotal);
     importe.innerText = sumaTotal;
-
   });
 
   let diving = document.getElementById("diving");
   let diving_date = document.getElementById("diving_date");
-  let pDivNum = document.getElementById("pDivNum");
-
+  let divJsonPiece;
+  let divJson;
   diving.addEventListener("click", (e) => {
     if (e.target.checked == true) {
       sumaTotal = sumaTotal + 45;
       diving_date.style.display = "block";
+      diving_date.addEventListener("change", (e) => {
+        divJsonPiece = {
+          "Date Time": diving_date.value,
+          "Initial Price": 50
+        };
+        divJson = {
+          "Diving": [divJsonPiece]
+        };
+        console.log(hourseJson);
+      });
     } else {
       sumaTotal = sumaTotal - 45;
       diving_date.style.display = "none";
+      divJson = {
+          "Diving": null
+        };
     }
     console.log(sumaTotal);
     importe.innerText = sumaTotal;
-
-  })
+  });
   let restaurant = document.getElementById("restaurant");
   let restaurant_date = document.getElementById("restaurant_date");
-  let pResNum = document.getElementById("pResNum");
 
   restaurant.addEventListener("click", (e) => {
     if (e.target.checked == true) {
@@ -154,7 +203,7 @@ include($_SERVER['DOCUMENT_ROOT'] . '/student034/dwes/header.php')
     }
     console.log(sumaTotal);
     importe.innerText = sumaTotal;
-  })
+  });
   importe.innerHTML = sumaTotal;
   // TODO Acabar los extras
   // En js creare un json que se guardara como extras y php lo leera para hacer el insert (siempre que no haya una manera de enviar los datos de manera mas facil claro)
