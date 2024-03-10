e3056e1c814355119b72a29c490378dbe85741dd
IvanCarreras <carreras.ivan.perez@gmail.com>
2024-03-10 11:47:03 +0100

    ahora si k si

--- db/rooms/db_rooms_select.php
+++ db/rooms/db_rooms_select.php
@@ -11,10 +11,10 @@ if (isset($_POST['submit'])) {
   $muestra = mysqli_fetch_all($resultado, MYSQLI_ASSOC);
 ?>
   <div class="d-flex justify-content-center align-items-center flex-wrap">
-    <?php foreach ($muestra as $room) {
-      showRoom($room,false,$user_role);
-    }
-  } ?>
+  <?php foreach ($muestra as $room) {
+    showRoom($room, false, $user_role, "", "", "");
+  }
+} ?>
   </div>
   <?php
   include($_SERVER['DOCUMENT_ROOT'] . '/student034/dwes/footer.php');
